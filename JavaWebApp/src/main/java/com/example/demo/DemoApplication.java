fa3e2e15c2e3edd0e0bf3bfd0eae8e92e4c735f8
ephraim mbube <mbube7@gmail.com>
2022-10-25 21:46:30 -0400

    bug fix

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -8,14 +8,12 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	
+
 		String name = "ewange";
 
 		System.out.println(name);
-
-		Integer age = "20";
-		System.out.println(age + 5);
 	}
 
 }
+
 
